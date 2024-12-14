64304eb4bd5df776c23d86599113772d86598e79
Daniel Hindrikes <daniel@hindrikes.se>
2024-12-14 17:34:12 +0100

    Add project files.

---
+++ StaticResourcesBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StaticResourcesBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ StaticResourcesBug/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace StaticResourcesBug
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
